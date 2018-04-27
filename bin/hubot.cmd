48caf210cc1938885ee6f0e371799621c51bce91
kzhrk <info@kzhrk.com>
2018-04-27 19:55:22 +0900

    updated hubot command

--- bin/hubot.cmd
+++ bin/hubot.cmd
@@ -1,7 +1,8 @@
 @echo off
 
 call npm install
+call npm run build
 SETLOCAL
 SET PATH=node_modules\.bin;node_modules\hubot\node_modules\.bin;%PATH%
 
-node_modules\.bin\hubot.cmd --name "bot-slack-google-calendar" %* 
+node_modules\.bin\hubot.cmd --name "bot-slack-google-calendar" %*
